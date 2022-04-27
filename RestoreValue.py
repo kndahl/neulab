0e5720804e3786e3fda8cb1c36897e1159212d9b
Roman Fitzjalen <eolika@Romans-MacBook-Pro.local>
2022-04-27 19:10:40 +0300

    add metrics

--- RestoreValue.py
+++ RestoreValue.py
@@ -2,21 +2,21 @@ import numpy as np
 from scipy import stats
 
 def MeanRestore(column):
-    '''Replace missing value with mean value. Returns value to restore.'''
+    '''Replace missing value with mean. Returns value to restore.'''
 
     vector = np.array(column)
     vector_mean = vector.mean()
     return vector_mean
 
 def MedianRestore(column):
-    '''Replace missing value with median value. Returns value to restore.'''
+    '''Replace missing value with median. Returns value to restore.'''
 
     vector = np.array(column)
     vector_median = np.median(vector)
     return vector_median
 
 def ModeRestore(column):
-    '''Replace missing value with mode value. Returns value to restore.''' 
+    '''Replace missing value with mode. Returns value to restore.''' 
 
     vector = np.array(column)
     mode_vector = stats.mode(vector)
