abafa0eea33af6a09627597966d9d2b38c77adec
Roman Fitzjalen <eolika@Romans-MacBook-Pro.local>
2022-04-28 17:27:40 +0300

    add readme in setup

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 distr:
+	python3 setup.py sdist
+
+distrwheel:
 	python3 setup.py bdist_wheel
 
 test:
--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='neulib',
     packages=find_packages(include=['neulib']),
-    version='0.1.0',
+    version='0.1.3',
     description='Tool for data preprocess in ML.',
     author='Roman Fitzjalen',
     author_email='romaactor@gmail.com',
     license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url = 'https://github.com/kndahl/neufarm',
+    include_package_data = True,
     install_requires=['numpy', 'pandas', 'scipy'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
