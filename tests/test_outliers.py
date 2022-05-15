6a9708862999e4c1eb2d9dad17793df344a27b02
Roman Fitzjalen <eolika@Romans-MacBook-Pro.local>
2022-05-15 20:58:59 +0300

    add test for qtest

--- tests/test_outliers.py
+++ tests/test_outliers.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('.')
-from neulab.OutlierDetection import SimpleOutDetect, Chauvenet, Quratile, DistQuant
+from neulab.OutlierDetection import SimpleOutDetect, Chauvenet, Quratile, DistQuant, DixonTest
 import pandas as pd
 import numpy as np
 
@@ -27,3 +27,9 @@ def test_metric():
     df = pd.DataFrame(data=d)
     df_ethalon = pd.DataFrame(data={'col1': [0, 1, 2, 4, 5, 5, 6], 'col2': [0, 1, 2, 0, 0, 1, 0]}, index=[1, 2, 3, 4, 5, 6, 7])
     pd.testing.assert_frame_equal(DistQuant(dataframe=df, metric='manhattan', filter='quantile', info=True, autorm=True), df_ethalon)
+
+def test_qtest():
+    d = {'col1': [2131, 180, 188, 177, 181, 185, 189], 'col2': [0, 0, 0, 0, 1, 13, 1]}
+    df = pd.DataFrame(data=d)
+    df_ethalon = pd.DataFrame(data={'col1': [180, 188, 177, 181, 189], 'col2': [0, 0, 0, 1, 1]}, index=[1, 2, 3, 4, 6])
+    pd.testing.assert_frame_equal(DixonTest(dataframe=df, q=95, info=True, autorm=True), df_ethalon)
